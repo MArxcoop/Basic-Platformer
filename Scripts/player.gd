dba211c69cec5d23f9de8012083b0374ef87bd09
MArxcoop <150504894+MArxcoop@users.noreply.github.com>
2026-02-02 11:34:39 -0500

    Animation, Camera, Player changes
    Animation: I added some of the animations and made a script to play them. Its a more complicated script because I wanted to make use of all the animations.
    
    Camera: I resized the camera in the test scene to make it smaller so that the player would fit nicely without sacrificing image quality.
    
    Player: I reduced player collision shape slightly and reduced player speed from 300 to 100
    
    Side note: I added a test input and set it to right click

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animation = $AnimatedSprite2D
+
+var dirString : String = "left"
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +16,45 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+		
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+		
+		
+		
+#region Animation code
+	# This code takes the last input pressed and sets the variable dirString to it
+	if Input.is_action_just_pressed("ui_left"):
+		animation.play("leftMove")
+		if dirString != "left":
+			dirString = "left"
+
+
+	elif Input.is_action_just_pressed("ui_right"):
+		animation.play("rightMove")
+		if dirString != "right":
+			dirString = "right"
+			
+	# if the player isn't moving then this code will check dirString to find 
+	# the animation it should play
+	if direction == 0:
+		if dirString == "left":
+			animation.play("idleLeft")
+		else:
+			animation.play("idleRight")
+#endregion
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if Input.is_action_just_pressed("test"):
+		print(velocity)
+		print(direction)
+		print(dirString[-1])
 
 	move_and_slide()
--- Scripts/resizableBlock.gd
+++ Scripts/resizableBlock.gd
@@ -4,16 +4,13 @@ var is_selected = false
 #Variables for scaling the box
 var x = 1
 var y = 1
-var min_size = .5
-var increment = .25
-var target_size = Vector2(x, y)
-var scale_speed = 10
 
 #The check to select the box
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		select_box()
 		get_viewport().set_input_as_handled()
+		print(x, y)
 
 #The check to deselect the box
 func _unhandled_input(event):
@@ -26,11 +23,13 @@ func select_box():
 	is_selected = true
 	modulate = Color(0.0, 0.4, 1.0, 1.0)
 	print("Box Selected")
+
 #Labels the box as deselected and reverts the color
 func deselect_box():
 	is_selected = false
 	modulate = Color(1,1,1)
 	print("Box Deselected")
+
 
 #Scales the box based off the input and if it is selected
 func _input(event):
@@ -38,22 +37,19 @@ func _input(event):
 		if event is InputEventKey and event.pressed:
 			if event.keycode == KEY_Q:
 				if event.shift_pressed:
-					if y > min_size:
-						y -= increment
+					if y > 1:
+						y -= 1
 					print("y down")
 				else:
-					y += increment
+					y += 1
 					print("y up")
 		if event is InputEventKey and event.pressed:
 			if event.keycode == KEY_E:
 				if event.shift_pressed:
-					if x > min_size:
-						x -= increment
+					if x > 1:
+						x -= 1
 					print("x down")
 				else:
-					x += increment
+					x += 1
 					print("x up")
-
-func _physics_process(delta):
-	target_size = Vector2(x, y)
-	scale = scale.lerp(target_size, scale_speed * delta)
+	scale = Vector2(x, y)
